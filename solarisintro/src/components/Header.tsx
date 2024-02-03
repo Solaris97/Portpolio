668a0471b6cd49ed617f09905074fba1a811096a
solaris97 <dlwjd15gus@naver.com>
2024-02-03 15:07:51 +0900

    ♻️ Refact : 로고 이미지 프리로드 추가

--- solarisintro/src/components/Header.tsx
+++ solarisintro/src/components/Header.tsx
@@ -42,8 +42,8 @@ const Header: React.FC<HeaderProps> = ({ handleMoveDownClick, IntroYn }) => {
                 {/* 반응형 */}
                 <div className="text-xl w-3/5 flex flex-row justify-center items-center text-center invisible md:visible">
                     <div className="w-1/5"><span className="cursor-pointer transition-colors duration-100 hover:text-orange-400" onClick={handleClick} id="About">About Me</span></div>
-                    <div className="w-1/5"><span className="cursor-pointer transition-colors duration-100 hover:text-orange-400" onClick={handleClick} id="Skills">Skills</span></div>
                     <div className="w-1/5"><span className="cursor-pointer transition-colors duration-100 hover:text-orange-400" onClick={handleClick} id="Projects">Projects</span></div>
+                    <div className="w-1/5"><span className="cursor-pointer transition-colors duration-100 hover:text-orange-400" onClick={handleClick} id="Skills">Skills</span></div>
                     <div className="w-1/5"><span className="cursor-pointer transition-colors duration-100 hover:text-orange-400" onClick={handleClick} id="Carrer">Carrer</span></div>
                     <div className="w-1/5"><span className="cursor-pointer transition-colors duration-100 hover:text-orange-400" onClick={handleClick} id="Archive">Archive</span></div>
                 </div>
@@ -62,8 +62,8 @@ const Header: React.FC<HeaderProps> = ({ handleMoveDownClick, IntroYn }) => {
                 <div className={`${IntroYn ? "font-baseFont bg-starBlack text-2xl text-white text-right bg-opacity-50 items-center pl-10 w-full mt-16 h-80 fixed shadow-xl z-40 visible md:invisible" :
                     "font-baseFont bg-white text-2xl text-black text-right bg-opacity-50 items-center pl-10 w-full mt-16 h-80 fixed shadow-xl z-40 visible md:invisible"}`}>
                     <div className="w-full h-16 flex justify-start items-center"><span className="cursor-pointer transition-colors duration-100 hover:text-orange-400" onClick={handleClick} id="About">About Me</span></div>
-                    <div className="w-full h-16 flex justify-start items-center"><span className="cursor-pointer transition-colors duration-100 hover:text-orange-400" onClick={handleClick} id="Skills">Skills</span></div>
                     <div className="w-full h-16 flex justify-start items-center"><span className="cursor-pointer transition-colors duration-100 hover:text-orange-400" onClick={handleClick} id="Projects">Projects</span></div>
+                    <div className="w-full h-16 flex justify-start items-center"><span className="cursor-pointer transition-colors duration-100 hover:text-orange-400" onClick={handleClick} id="Skills">Skills</span></div>
                     <div className="w-full h-16 flex justify-start items-center"><span className="cursor-pointer transition-colors duration-100 hover:text-orange-400" onClick={handleClick} id="Carrer">Carrer</span></div>
                     <div className="w-full h-16 flex justify-start items-center"><span className="cursor-pointer transition-colors duration-100 hover:text-orange-400" onClick={handleClick} id="Archive">Archive</span></div>
                 </div>
